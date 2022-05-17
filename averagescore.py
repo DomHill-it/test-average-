1b90002c13be4c9d98a309ae5435afa812e00ce9
Dominique Hill <69709885+DomHill-it@users.noreply.github.com>
2022-05-17 16:54:11 -0400

    Add files via upload

---
+++ averagescore.py
@@ -0,0 +1,10 @@
+#Test Averages written by Dominique Hill
+
+#Define Variables
+Test1=80
+Test2=70
+Test3=90
+Average=Test1+Test2+Test3/3
+
+#Display Answer
+print("Your calculated Average is",Average)
